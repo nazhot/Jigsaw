a014a0f6b0f16d2e317ce6416d138f5ca74cfdb9
nazhot <15216154+nazhot@users.noreply.github.com>
2025-01-16 06:42:22 -0700

    Added exit to Main
    Just added it so I don't have to manually stop the program every time I
    run it while I'm testing out the new edge generation method

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@ int main( int argc, char *argv[] ) {
     for ( uint i = 1; i <= 20; ++i ) {
         generateEdge( i );
     }
+    exit( 0 );
+
     benchmark_puzzleSolve( 50000, "Initial PuzzleSolve, no modifications, no debug, level 3 optimizations" );
 
     exit( 0 );
